e8c264a1d503e92c7b57ebe9af359bfc8d497edc
Alex Schedler <con.sume.org+github@googlemail.com>
2018-10-31 15:23:36 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker run \
     -p 5984:5984 \
     --volume /opt/couchdb/data:/opt/couchdb/data \
     --volume /opt/couchdb/config:/opt/couchdb/etc/local.d/ \
-    --volume /opt/couchdb/logs:/opt/couchdb/var/log/ \
+    --volume /opt/couchdb/logs:/opt/couchdb/logs/ \
     --name arm-couch \
     --env COUCHDB_USER=admin \
     --env COUCHDB_PASSWORD=password
